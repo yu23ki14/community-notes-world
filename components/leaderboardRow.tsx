f8867707478e7c06fe2a7d5f00afe8dd5f76de97
Lucas Neumann de Antonio <lucasneumann.fau@gmail.com>
2022-11-08 10:37:24 -0800

    Update leaderboard icons Fixes #6

--- components/leaderboardRow.tsx
+++ components/leaderboardRow.tsx
@@ -1,30 +1,50 @@
 import { styled } from "@stitches/react";
 import { author, note } from "../utils/types";
+import Trophy from "../public/icons/trophy.svg";
+import { copyFile } from "fs";
 const Row = styled("div", {
   display: "flex",
-  justifyContent: "space-between",
   width: "100%",
   padding: "1rem",
   borderTop: "1px solid dimGray",
+  justifyContent: "space-between",
 });
+const AuthorWrapper = styled("div", {
+  display: "flex",
+});
+
 type props = {
   author: author;
   index: number;
 };
 const leaderBoardRow = ({ author, index }: props) => {
-  const medal =
-    index === 0 ? "🥇" : index === 1 ? "🥈" : index === 2 ? "🥉" : "⚫️";
+  const color =
+    index === 0
+      ? "orange"
+      : index === 1
+      ? "gray"
+      : index === 2
+      ? "brown"
+      : "#333";
   return (
     <Row key={author.participantId}>
-      <div>
+      <AuthorWrapper>
+        <Trophy
+          width={"24px"}
+          height={"24px"}
+          viewBox="0 0 48 48"
+          color={color}
+          style={{ marginRight: "12px" }}
+        />
         <a
           target="_blank"
           rel="noreferrer"
           href={`https://twitter.com/i/birdwatch/n/${author.notes[1].noteId}`}
         >
-          {medal} Top writer nº{index + 1}
+          Top writer nº
+          {index + 1}
         </a>
-      </div>
+      </AuthorWrapper>
       <div>{author.numberOfHelpfulNotes} helpful notes</div>
     </Row>
   );
