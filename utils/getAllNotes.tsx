731acbb561a96a26743d6deab45c1bf504a2d776
Lucas Neumann de Antonio <lucasneumann.fau@gmail.com>
2022-11-11 17:11:00 -0800

    refactor

--- utils/getAllNotes.tsx
+++ utils/getAllNotes.tsx
@@ -7,36 +7,27 @@ import {
 } from "./dates";
 export default async function getAllHelpfulNotes() {
   const noteStatusHistoryUrl = `https://ton.twimg.com/birdwatch-public-data/${currentYear}/${currentMonthFormatted}/${currentDayFormatted}/noteStatusHistory/noteStatusHistory-00000.tsv`;
-  const notesUrl = `https://ton.twimg.com/birdwatch-public-data/${currentYear}/${currentMonthFormatted}/${currentDayFormatted}/notes/notes-00000.tsv`;
 
   const res = await fetch(noteStatusHistoryUrl);
   const text = await res.text();
-
-  const helpfulNotes = parse(text, {
-    columns: true,
-    skip_empty_lines: true,
-    delimiter: "\t",
-  }).filter((item: note) => item.currentStatus === "CURRENTLY_RATED_HELPFUL");
-
-  const notHelpfulNotes = parse(text, {
-    columns: true,
-    skip_empty_lines: true,
-    delimiter: "\t",
-  }).filter(
-    (item: note) => item.currentStatus === "CURRENTLY_RATED_NOT_HELPFUL"
-  );
-
-  const needsMoreRatingsNotes = parse(text, {
-    columns: true,
-    skip_empty_lines: true,
-    delimiter: "\t",
-  }).filter((item: note) => item.currentStatus === "NEEDS_MORE_RATINGS");
 
   const allNotes = parse(text, {
     columns: true,
     skip_empty_lines: true,
     delimiter: "\t",
   });
+
+  const helpfulNotes = allNotes.filter(
+    (item: note) => item.currentStatus === "CURRENTLY_RATED_HELPFUL"
+  );
+
+  const notHelpfulNotes = allNotes.filter(
+    (item: note) => item.currentStatus === "CURRENTLY_RATED_NOT_HELPFUL"
+  );
+
+  const needsMoreRatingsNotes = allNotes.filter(
+    (item: note) => item.currentStatus === "NEEDS_MORE_RATINGS"
+  );
 
   const notes = {
     allNotes: allNotes,
