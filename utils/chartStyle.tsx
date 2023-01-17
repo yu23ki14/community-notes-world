f04a71c8324f77f1135f8c402f080a65fcf7d7eb
Lucas Neumann de Antonio <lucasneumann.fau@gmail.com>
2023-01-17 13:38:17 -0800

    ticks

--- utils/chartStyle.tsx
+++ utils/chartStyle.tsx
@@ -1,4 +1,28 @@
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dez",
+];
 export const chartConfig = {
+  scales: {
+    x: {
+      ticks: {
+        callback: function (value: any, index: number, ticks: any): any {
+          return months[Math.floor(this.getLabelForValue(value).slice(5)) - 1];
+        },
+        maxRotation: 0,
+      },
+    },
+  },
   elements: {
     point: {
       radius: 0,
